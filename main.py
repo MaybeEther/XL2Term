a7fe2e9a969463a3ced9164eaaec9313433c4d5f
MaybeEther <75216074+MaybeEther@users.noreply.github.com>
2022-07-28 10:18:45 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import pandas as pd
 import xlrd
-def TooTerm(File):
-    FILE = File
-    df = pd.read_excel(FILE)
-    FINAL = ("+")
-    DASHES = ""
-    SPACES = ""
+def TooTerm(file):
+    data = file
+    df = pd.read_excel(data)
+    final = "+"
+    dashes = ""
+    spaces = ""
     largest = ""
     content = ("")
-
-    cellY = 0
-    cellX = 0
+    celly = 0
+    cellx = 0
     biggest = 0
-    wb = xlrd.open_workbook(FILE)
+    wb = xlrd.open_workbook(data)
     sheet = wb.sheet_by_index(0)
-
     if df.empty:
         print("This File is Empty!!!")
     else:
@@ -23,20 +21,20 @@ def TooTerm(File):
             if y > biggest:
                 biggest = y
         for x in range(biggest):
-            DASHES = (DASHES + "-")
+            dashes = (dashes + "-")
         for x in list(df):
-            FINAL = (FINAL + DASHES + "+")
-        MegaFinale = (FINAL)
+            final = (final + dashes + "+")
+        megafinale = (final)
         for x in range(df.shape[0] + 1):
             for x in range(df.shape[1]):
-                SPACES = biggest - len(str(sheet.cell_value(cellX, cellY)))
-                for x in range(SPACES):
+                spaces = biggest - len(str(sheet.cell_value(cellx, celly)))
+                for x in range(spaces):
                     largest = largest + " "
-                content = content + str(sheet.cell_value(cellX, cellY)) + largest + "|"
+                content = content + str(sheet.cell_value(cellx, celly)) + largest + "|"
                 largest = ""
-                cellY = cellY + 1
-            cellX = cellX + 1
-            cellY = 0
-            MegaFinale = MegaFinale + "\n|" + content + "\n" + FINAL
+                celly = celly + 1
+            cellx = cellx + 1
+            celly = 0
+            megafinale = megafinale + "\n|" + content + "\n" + final
             content = ("")
-        print(MegaFinale)
+        print(megafinale)
